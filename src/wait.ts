9baccc2d86702701fa1b79709db673501840b874
Aaron Franks <aaron.franks@gmail.com>
2020-08-09 14:06:39 -0700

    log some stuff

--- src/wait.ts
+++ src/wait.ts
@@ -1,4 +1,13 @@
+/* eslint-disable no-console */
+import * as cp from 'child_process'
+
 export async function wait(milliseconds: number): Promise<string> {
+  console.log('PRINTING LS')
+  console.log(cp.execSync('ls -alh', {encoding: 'utf8'}))
+
+  console.log('PRINTING PWD')
+  console.log(cp.execSync('pwd', {encoding: 'utf8'}))
+
   return new Promise(resolve => {
     if (isNaN(milliseconds)) {
       throw new Error('milliseconds not a number')
