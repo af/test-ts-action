447b7c721b4b1b34b05a1af70968cc2f6883e247
Aaron Franks <aaron.franks@gmail.com>
2020-08-09 14:49:36 -0700

    misc wip v3

--- src/wait.ts
+++ src/wait.ts
@@ -1,5 +1,13 @@
 /* eslint-disable no-console */
 import * as cp from 'child_process'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as core from '@actions/core'
+import TOML from '@iarna/toml'
+
+// TODO: getInput() doesn't seem to get the yaml default value during tests?
+const configName = core.getInput('config_file') || 'myconfig.toml'
+const configPath = path.join(process.cwd(), configName)
 
 export async function wait(milliseconds: number): Promise<string> {
   console.log('PRINTING LS')
@@ -7,6 +15,12 @@ export async function wait(milliseconds: number): Promise<string> {
 
   console.log('PRINTING PWD')
   console.log(cp.execSync('pwd', {encoding: 'utf8'}))
+
+  console.log('CONFIG PATH IS', configName, configPath)
+  const parsedConfig = TOML.parse(fs.readFileSync(configPath, 'utf8'))
+
+  console.log('PRINTING PARSED CONFIG')
+  console.log(JSON.stringify(parsedConfig, null, 2))
 
   return new Promise(resolve => {
     if (isNaN(milliseconds)) {
